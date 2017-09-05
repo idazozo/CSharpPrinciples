25a4347f9ecc7aabaabfc1ff7867cbb4ae367058
shenshuh <shuheng.shen@icbcstandard.com>
2017-09-05 10:08:34 +0100

    First Commit

---
+++ NullableAtShow/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace CalculatorApplication
+{
+    class NullableAtShow
+    {
+        // Syntax for declaring a nullable type is as follows:
+        // <data_type> ? <variable_name> = null/value;
+
+        static void Main(string[] args)
+        {
+            int? num1 = null;
+            int? num2 = 45;
+            double? num3 = new double?();
+            double? num4 = 3.14157;
+            Console.WriteLine("Nullables at Show: {0}, {1}, {2}, {3}", num1, num2, num3, num4);
+
+            bool? boolval = new bool?();
+            Console.WriteLine("A Nullable boolean value: {0}", boolval);
+
+            // Null Coalescing Operator (??)
+            // If the value of the first oprand is null, then the operator 
+            // returns ther value of the second sperand, otherwise it returns
+            // thr value of the first operand.
+            double num6;
+            num6 = num1 ?? 5.34;
+            Console.WriteLine("Value of num6: {0}", num6);
+
+            num6 = num4 ?? 5.34;
+            Console.WriteLine("Value of num3: {0}", num6);
+
+            // diapley the values
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StringApplication/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringApplication
+{
+    class StringPro
+    {
+        static void Main(string[] args)
+        {
+            // Comparing Strings:
+            string str1 = "This is test";
+            string str2 = "This is text";
+
+            if (String.Compare(str1, str2) == 0)
+            {
+                Console.WriteLine(str1 + " and " + str2 + " are equal.");
+            }
+            else
+            {
+                Console.WriteLine(str1 + " and " + str2 + " are not equal.");
+            }
+
+            // String contains String:
+            if (str1.Contains("test"))
+            {
+                Console.WriteLine("The sequence 'test' was found.");
+            }
+
+            // Getting a Substring:
+            string str = "Last night I dreamt of San Pedro";
+            string substr = str.Substring(23);
+            Console.WriteLine("The substring from position 23 is " + substr);
+
+            // Joining Strings:
+            string[] starray = new string[]{"Down the way nights are dark",
+                     "And the sun shines daily on the mountain top",
+                     "I took a trip on a sailing ship",
+                     "And when I reached Jamaica",
+                     "I made a stop"};
+
+            string str3 = String.Join("\n", starray);
+            Console.WriteLine(str3);
+
+            Console.ReadKey();
+        }
+    }
+}
