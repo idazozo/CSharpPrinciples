75cb55700a463a0c1a5fd1544adeb4917044ba95
shenshuh <shuheng.shen@icbcstandard.com>
2017-10-18 13:10:56 +0100

    Use of Delegates

---
+++ DelegateApplication/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+delegate int NumberChanger(int n);
+namespace DelegateApplication
+{
+    class TestDelegate
+    {
+        static int num = 10;
+        public static int AddNum(int p)
+        {
+            num += p;
+            return num;
+        }
+
+        public static int MultNum(int q)
+        {
+            num *= q;
+            return num;
+        }
+
+        public static int getNum()
+        {
+            return num;
+        }
+
+        static void Main(string[] args)
+        {
+            // Create delegate instances
+            NumberChanger nc;
+            NumberChanger nc1 = new NumberChanger(AddNum);
+            NumberChanger nc2 = new NumberChanger(MultNum);
+
+            // Multicasting of a delegate
+            nc = nc1;
+            nc += nc2;
+
+            // Calling multicast
+            nc(5);
+            Console.WriteLine($"Value of Num: {getNum()}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UsingDelegates/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DelegateApplication
+{
+    class PrintString
+    {
+        static FileStream fs;
+        static StreamWriter sw;
+
+        // Delegate declaration
+        public delegate void printString(string s);
+
+        // This method prints to the console
+        public static void WriteToScreen(string str)
+        {
+            Console.WriteLine($"The String is: {str}");
+        }
+
+        //This method prints to a file
+        public static void WriteToFile(string s)
+        {
+            fs = new FileStream("c:\\Temp\\message.txt",
+                FileMode.Append, FileAccess.Write);
+            sw = new StreamWriter(fs);
+            sw.WriteLine(s);
+            sw.Flush();
+            sw.Close();
+            fs.Close();
+        }
+
+        // This method takes the delegate as parameter and uses it to 
+        // call the methods as required
+        public static void sendString(printString ps)
+        {
+            ps("Hello World");
+        }
+
+        static void Main(string[] args)
+        {
+            printString ps1 = new printString(WriteToScreen);
+            printString ps2 = new printString(WriteToFile);
+            sendString(ps1);
+            sendString(ps2);
+            Console.ReadKey();
+        }
+    }
+}
